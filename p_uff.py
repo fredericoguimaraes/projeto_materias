ae00ebd87b5bfd982b5035ca6692ed761ff97712
fredericoguimaraes <82128682+fredericoguimaraes@users.noreply.github.com>
2021-10-04 20:06:02 -0300

    Update p_uff.py

--- p_uff.py
+++ p_uff.py
@@ -14,7 +14,7 @@ def ler_dados(curso):
 
 
 cursos = ['agricola' ,"civil","eletrica","mecanica","petroleo","producao","quimica","rec_hidricos","telecom"]
-st.subtitle('Matérias por turno para cada curso')
+st.write('Matérias por turno para cada curso')
 curso_selecionado = st.selectbox('Cursos',cursos)
 
 
