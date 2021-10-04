fda9778470c1c5a7fdeeaeaf6e382cd1a3370aa0
fredericoguimaraes <82128682+fredericoguimaraes@users.noreply.github.com>
2021-10-04 20:35:22 -0300

    Update p_uff.py

--- p_uff.py
+++ p_uff.py
@@ -171,7 +171,7 @@ layout = go.Layout(
     paper_bgcolor='rgba(0,0,0,0)',
     plot_bgcolor='rgba(0,0,0,0)')
 base_g2['valor2'] = ["{0:.2f}%".format(i*100) for i in base_g2["valor"]]
-fig2 = go.Figure(go.Bar(x=base_g2['valor'],y=base_g2['curso'],orientation='h',text=base_g2['valor2']))
+fig2 = go.Figure(go.Bar(x=base_g2['valor'],y=base_g2['curso'],orientation='h',text=base_g2['valor2'],textposition="auto"))
 fig2.update_xaxes(visible=False)
 fig2.update_yaxes(visible=True)
 fig2.update_layout(title="Ranking de Cursos por Turno")
