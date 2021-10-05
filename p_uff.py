92ae53ca2e3d0764c41b2728dac3ddad48ea81dd
fredericoguimaraes <82128682+fredericoguimaraes@users.noreply.github.com>
2021-10-04 22:21:47 -0300

    Update p_uff.py

--- p_uff.py
+++ p_uff.py
@@ -7,6 +7,10 @@ import streamlit as st
 
 st.sidebar.title("Projeto- Engenharia UFF")
 st.sidebar.header("       2021.2")
+st.sidebar.write(" ")
+st.sidebar.write(" ")
+st.sidebar.write(" ")
+st.sidebar.write("Criado por: Frederico Guimarães")
 def ler_dados(curso):
     curso = curso + '.xlsx'
     base = pd.read_excel(curso)
